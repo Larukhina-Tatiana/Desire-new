a13c39f189fd118ab15ac815d9ce7cd003dfec01
Татьяна <ltasya72@gmail.com>
2025-03-22 21:11:27 +0200

    modified: blog-one, comments-form.js, comments.scss

--- js/comments-form.js
+++ js/comments-form.js
@@ -1,18 +1,15 @@
-if (document.querySelector(".comments__form")) {
-  const formComments = document.querySelector(".comments__form");
-  const STORAGE_KEY = "comments-subscribe";
-  let formData = {};
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.querySelector(".comments__form")) {
+    const formComments = document.querySelector(".comments__form");
+    const STORAGE_KEY = "comments-subscribe";
+    let formData = {};
 
-  formComments.addEventListener("submit", onFormSubmit);
-  formComments.addEventListener("input", onFormInput);
+    formComments.addEventListener("submit", onFormSubmit);
+    formComments.addEventListener("input", onFormInput);
 
-  function onFormSubmit(event) {
-    event.preventDefault(); //предотвращает перезагрузку(обновление страниы)
+    function onFormSubmit(event) {
+      event.preventDefault(); //предотвращает перезагрузку(обновление страниы)
 
-    if (event.currentTarget.elements.email.value === "") {
-      alert("The field must be filled`");
-    } else {
-      event.preventDefault();
       formComments.reset();
       localStorage.removeItem(STORAGE_KEY);
 
@@ -24,26 +21,29 @@ if (document.querySelector(".comments__form")) {
         alertBox.remove();
       }, 3000);
     }
-  }
 
-  function onFormInput(event) {
-    formData[event.target.name] = event.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  }
-
-  (function restoreFormOutput() {
-    if (localStorage.getItem(STORAGE_KEY)) {
-      formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-      console.log(formData);
-      if (
-        formComments.elements[key].type === "chekbox" &&
-        formComments.elements[key].value === "on"
-      ) {
-        formComments.elements[key].checked = true;
+    function onFormInput(event) {
+      if (event.target.type === "checkbox") {
+        formData[event.target.name] = event.target.checked;
       } else {
-        formComments.elements[key].value = formData[key];
+        formData[event.target.name] = event.target.value;
       }
+
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
     }
-    т;
-  })();
-}
+
+    (function restoreFormOutput() {
+      if (localStorage.getItem(STORAGE_KEY)) {
+        formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        console.log(formData);
+        for (let key in formData) {
+          if (formComments.elements[key].type === "checkbox" && formData[key]) {
+            formComments.elements[key].checked = true;
+          } else {
+            formComments.elements[key].value = formData[key];
+          }
+        }
+      }
+    })();
+  }
+});
