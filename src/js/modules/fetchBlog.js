de61f8d61f5fa8ee4d3877f174efedf6f21a3c4c
Татьяна <ltasya72@gmail.com>
2026-02-02 10:54:01 +0200

    Переработана логика вставки элементов в список блогов для обработки наличия цитат. - Добавлена ​​логика проверки наличия элемента «цитата» в списке блогов. - Если цитата найдена, новые элементы блога вставляются перед ней; в противном случае они добавляются в конец списка.

--- src/js/modules/fetchBlog.js
+++ src/js/modules/fetchBlog.js
@@ -4,8 +4,10 @@ import { renderArticle } from "../utils/utils.js";
 export function initBlogList(blogs, options = { full: false }) {
   const list = document.querySelector(".blog__items");
   if (!list || !Array.isArray(blogs)) return;
+
+  // ищем блокquote, если он есть
   const blockquote = list.querySelector(".blog__blockquote");
-  const next = blockquote.nextSibling;
+  const insertBeforeNode = blockquote ? blockquote.nextSibling : null;
   // const saved = blockquote.cloneNode(true);
   // Очистить контейнер, чтобы не копить карточки
   // list.innerHTML = "";
@@ -19,5 +21,10 @@ export function initBlogList(blogs, options = { full: false }) {
     item.innerHTML = html;
     fragment.appendChild(item);
   });
-  list.insertBefore(fragment, next);
+  // если есть blockquote → вставляем перед ним если нет → добавляем в конец списка
+  if (insertBeforeNode) {
+    list.insertBefore(fragment, insertBeforeNode);
+  } else {
+    list.appendChild(fragment);
+  }
 }
